313d007e9e075eacae35abc45ee1e1f5e80913a7
René Magnet <rene.m.magnet@gmail.com>
2022-09-21 19:17:25 +0200

    add f

--- Javascript Day 1 Rene Gloria/Intermediate_Exercise_3_Rene/script.js
+++ Javascript Day 1 Rene Gloria/Intermediate_Exercise_3_Rene/script.js
@@ -5,5 +5,5 @@ let fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
 document.write(fruits.split("/").join("<br>"));
 
 // solution 2
-// document.write(fruits.replaceAll("/", "<br>"));
+//document.write(fruits.replaceAll("/", "<br>"));
 
