09cc4d7a065b1ad6ddad7425ab55d36c2f30165c
René Magnet <rene.m.magnet@gmail.com>
2022-09-22 10:38:40 +0200

    add f

---
+++ Javascript Day 2/script.js
@@ -0,0 +1,10 @@
+let randomNum = Math.floor(Math.random() * 11);
+
+console.log(randomNum);
+
+
+//function getRandomArbitrary(min, max) {
+ //   return Math.random() * (25 - -5) + -5;
+ // }
+
+  // console.log(Math.random);
