2f1f00241e5e62ad198961ddf9bed043cf13879a
René Magnet <rene.m.magnet@gmail.com>
2022-09-26 10:54:51 +0200

    add changes

---
+++ GENERAL/script-basic-exercise.js
@@ -0,0 +1,9 @@
+
+function max(input) {
+    if (toString.call(input) !== "[object Array]")  
+      return false;
+ return Math.max.apply(null, input);
+   }
+
+ console.log(max([12,34,56,1,-22,58]));
+console.log(max([-12,-34,0,-56,-1]));
